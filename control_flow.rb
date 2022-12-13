3a84e9510abcf4c88197c951ea55722b4842f784
Keith Ma <keithchezzy@gmail.com>
2022-12-12 19:51:44 -0800

    commit tree 96379201a29ba147caf9206d8802d6e6eb621c3f

--- control_flow.rb
+++ control_flow.rb
@@ -7,28 +7,28 @@ def admin_login(username, password)
 end
 def hows_the_weather(temperature)
   response =  if temperature < 40
-                "brisk"
-              elsif temperature >= 40 && temperature <= 65
-                "a little chilly"
-              elsif temperature > 85
-                "too dang hot"
-              else
-                "perfect"
-              end
+    "brisk"
+  elsif temperature >= 40 && temperature <= 65
+    "a little chilly"
+  elsif temperature > 85
+    "too dang hot"
+  else
+    "perfect"
+  end
 
-  "It's #{response} out there!"
+"It's #{response} out there!"
 end
 def fizzbuzz(num)
   # your code here
-  if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
-  elsif num % 3 === 0
-    "Fizz"
-  elsif num % 5 === 0
-    "Buzz"
-  else 
-    num
-  end
+ if num % 3 === 0 && num % 5 === 0
+  "FizzBuzz"
+ elsif num % 3 === 0
+  "Fizz"
+ elsif num % 5 === 0
+  "Buzz"
+ else
+  num
+ end
 end
 
 def calculator(operation, num1, num2)
@@ -42,8 +42,8 @@ def calculator(operation, num1, num2)
     num1 * num2
   when "/"
     num1 / num2
-  else 
-    puts "Invalid operation!"
+  else
+   puts "Invalid operation!"
   end
 end
 
